470dd82abd43051a6a50af2bad3804f0eed52646
killeen31 <j31killeen@gmail.com>
2023-06-08 14:26:26 -0400

    finished webpack and updated small changes

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,7 +4,7 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.defferedPrompt = event;
-    butInstall.classList.toggle('hidden', flase)
+    butInstall.classList.toggle('hidden', false)
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
